6298363496063694ccb0820f9ca677cb147fec89
n_mckinnon2000 <n_mckinnon2000@yahoo.co.uk>
2017-01-26 22:31:11 +0000

    removed comments

--- src/main/java/com/nmt/fx/rate/DemoApplication.java
+++ src/main/java/com/nmt/fx/rate/DemoApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	asd
-	a
-	sdadad
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
