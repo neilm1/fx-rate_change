cc075c40167af03c6d37456008427234ade8ee14
n_mckinnon2000 <n_mckinnon2000@yahoo.co.uk>
2017-01-26 22:49:02 +0000

    added comment

--- src/main/java/com/nmt/fx/rate/DemoApplication.java
+++ src/main/java/com/nmt/fx/rate/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // add comment
+// another comment
+// one more comment
 
 @SpringBootApplication
 public class DemoApplication {
